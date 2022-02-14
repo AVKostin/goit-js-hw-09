a6cd35ec5bf63d3aed6cb269ae067d2902ff685d
AVKostin <alexander.kostin.ua@gmail.com>
2022-02-14 08:33:08 +0200

    color switcher start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const body = document.querySelector('body');
+const btnStartRef = document.querySelector('[data-start]');
+const btnStopRef = document.querySelector('[data-stop]');
+let timerId = null;
+let isActive = false;
 
+const onStartRandomBGColor = () => {
+	if (isActive) {
+		return;
+	}
+	isActive = true;
+
+	body.style.backgroundColor = getRandomHexColor();
+
+	timerId = setInterval(() => {
+		body.style.backgroundColor = getRandomHexColor();
+	}, 1000);
+};
+
+function onStopChangeBGColor() {
+	clearInterval(timerId);
+	isActive = false;
+}
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStartRef.addEventListener('click', onStartRandomBGColor);
+btnStopRef.addEventListener('click', onStopChangeBGColor);
